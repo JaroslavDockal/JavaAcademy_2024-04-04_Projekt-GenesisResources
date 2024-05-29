c9b5e0338319fcceacfffdadd72a9da486e4b13a
JaroslavDockal <157100203+JaroslavDockal@users.noreply.github.com>
2024-05-29 12:59:50 +0100

    Pořád nefunguje

--- src/main/java/com/engeto/ja/genesisResources/GenesisResourcesApplication.java
+++ src/main/java/com/engeto/ja/genesisResources/GenesisResourcesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.engeto.ja.genesisResources.model.User")
+@EntityScan("com.engeto.ja.genesisResources.model")
 public class GenesisResourcesApplication {
 
 	public static void main(String[] args) {
