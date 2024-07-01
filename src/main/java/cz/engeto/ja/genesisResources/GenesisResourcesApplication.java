1dc6e5048cd14d8610e23d489953e16f7412b971
JaroslavDockal <157100203+JaroslavDockal@users.noreply.github.com>
2024-07-01 11:43:50 +0200

    Přidáno logování běhu aplikace

--- src/main/java/cz/engeto/ja/genesisResources/GenesisResourcesApplication.java
+++ src/main/java/cz/engeto/ja/genesisResources/GenesisResourcesApplication.java
@@ -1,13 +1,46 @@
 package cz.engeto.ja.genesisResources;
 
+import cz.engeto.ja.genesisResources.util.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"cz.engeto.ja.genesisResources"})
 public class GenesisResourcesApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(GenesisResourcesApplication.class, args);
+		SpringApplication app = new SpringApplication(GenesisResourcesApplication.class);
+		app.addListeners(new ApplicationStartingListener());
+		app.run(args);
+	}
+
+	@Component
+	public static class ApplicationStartingListener implements ApplicationListener<ApplicationStartingEvent> {
+		@Override
+		public void onApplicationEvent(ApplicationStartingEvent event) {
+			Logger.log("Genesis Resources Application starting.");
+		}
+	}
+
+	@Component
+	public static class ApplicationEventListener implements ApplicationListener<ContextRefreshedEvent> {
+		@Override
+		public void onApplicationEvent(ContextRefreshedEvent event) {
+			Logger.log("Genesis Resources Application started.");
+		}
+	}
+
+	@Component
+	public static class ApplicationShutdownListener implements ApplicationListener<ContextClosedEvent> {
+		@Override
+		public void onApplicationEvent(ContextClosedEvent event) {
+			Logger.log("Genesis Resources Application shutting down.");
+		}
 	}
 }
