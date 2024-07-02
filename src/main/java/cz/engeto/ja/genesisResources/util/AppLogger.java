3f84bbdf7b8e8c6214a4745988b647465ec9972b
JaroslavDockal <157100203+JaroslavDockal@users.noreply.github.com>
2024-07-02 13:24:12 +0200

    Definována severita eventů

--- src/main/java/cz/engeto/ja/genesisResources/util/AppLogger.java
+++ src/main/java/cz/engeto/ja/genesisResources/util/AppLogger.java
@@ -11,10 +11,27 @@ public class AppLogger {
 
     /**
      * Logs a message with INFO level.
-     *
      * @param message The message to be logged
      */
-    public static void log(String message) {
+    public static void info(String message) {
         logger.info(message);
     }
+
+    /**
+     * Logs a message with WARN level.
+     * @param message The warn message to be logged
+     */
+    public static void warn(String message) {
+        logger.warn(message);
+    }
+
+    /**
+     * Logs a message with ERROR level.
+     * @param message The error message to be logged
+     */
+    public static void error(String message) {
+        logger.error(message);
+    }
+
+
 }
